7c3da84d565846c643259a17c24f7ec5268ecd1d
Asa Alger <asa@oxbowcreative.com>
2017-08-18 13:36:35 -0400

    included exec

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var CUR_SERVER = "fritzdavenport.com";
 
 var restify = require('restify');
 var request = require('request');
+var child_process = require('child_process');
 
 const server = restify.createServer({
   name: 'myapp',
